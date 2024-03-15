c7fa96cf44ad7ae7313022f8e03df66c9018fd12
taerim.woo <taerim.woo@quantum-ai.ai>
2024-03-15 10:40:16 +0900

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `${appUrl}/api/v1`,
+        target: `${appUrl}/api`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
